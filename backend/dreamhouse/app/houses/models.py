c3cb84f4f6fcdc3ae68a5327c2223d810216df63
Salmu10 <Ubedasalmu@gmail.com>
2023-05-08 21:28:28 +0200

    cambios en el modelo

--- backend/dreamhouse/app/houses/models.py
+++ backend/dreamhouse/app/houses/models.py
@@ -4,13 +4,16 @@ from dreamhouse.app.users.models import User
 class Category(models.Model):
     slug = models.SlugField(max_length=100, unique=True, editable=False)
     name = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
 
     def __str__(self):
         return str(self.id)
 
 class House(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, related_name="category")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
     image = models.ImageField(upload_to='main_image')
+    price = models.IntegerField()
     country = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
@@ -19,11 +22,6 @@ class House(models.Model):
 
     def __str__(self):
         return str(self.id)
-    
-class Belongs(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, related_name="category")
-    house = models.ForeignKey(House, on_delete=models.CASCADE, null=False, related_name="house")
-    price = models.IntegerField()
     
 class HouseServices(models.Model):
     house = models.ForeignKey(House, on_delete=models.CASCADE, null=False, related_name="house_services")

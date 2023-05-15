bb8ad2f6aba2556053f477f76653383512c0070f
Salmu10 <Ubedasalmu@gmail.com>
2023-05-15 21:10:46 +0200

    minor changes

--- frontend/src/secrets.js
+++ frontend/src/secrets.js
@@ -2,7 +2,8 @@ const secrets = {
     URL_DRF: "http://localhost:8000/api/",
     URL_BACKEND: "http://localhost:8000",
     // URL_DRF: "http://172.20.10.3:8000/api/"
-    // URL_DRF: "http://192.168.0.26:8000/api/"
+    // URL_DRF: "http://192.168.0.26:8000/api/",
+    // URL_BACKEND: "http://192.168.0.26:8000"
 }
 
 export default secrets;

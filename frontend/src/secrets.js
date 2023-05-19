9801f47eba2d70e800720a09449fb15714c6d5c2
Salmu10 <Ubedasalmu@gmail.com>
2023-05-19 12:18:23 +0200

    cambios para el despliegue

--- frontend/src/secrets.js
+++ frontend/src/secrets.js
@@ -1,6 +1,8 @@
 const secrets = {
-    URL_DRF: "http://localhost:8000/api/",
-    URL_BACKEND: "http://localhost:8000",
+    // URL_DRF: "http://localhost:8000/api/",
+    // URL_BACKEND: "http://localhost:8000",
+    URL_DRF: "http://98.66.137.156:8000/api/",
+    URL_BACKEND: "http://98.66.137.156:8000",
     // URL_DRF: "http://172.20.10.3:8000/api/"
     // URL_DRF: "http://192.168.0.26:8000/api/",
     // URL_BACKEND: "http://192.168.0.26:8000"
